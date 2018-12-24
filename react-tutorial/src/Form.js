fcc6b81a771e3c98db02f6a52a2f401cb5a6498b
ADSherman <adam.sherman494@gmail.com>
2018-12-24 10:14:31 +1000

    Form working

--- react-tutorial/src/Form.js
+++ react-tutorial/src/Form.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 
-	class Form extends Component {
+class Form extends Component {
 
-		constructor(props) {
-			super(props);
+	handleChange = event => {
+    const {name, value} = event.target;
 
-			this.initialState = {
-				name: '',
-				job: ''
-			};
+    this.setState({
+        [name] : value
+    });
+	}
 
-			this.state = this.initialState;
+	constructor(props) {
+		super(props);
 
-		}
+		this.initialState = {
+			name: '',
+			job: ''
+		};
+
+		this.state = this.initialState;
 
 	}
+
+	render(){
+	  const { name, job } = this.state; 
+
+	  return (
+      <form>
+        <label>Name</label>
+        <input 
+          type="text" 
+          name="name" 
+          value={name} 
+          onChange={this.handleChange} />
+        <label>Job</label>
+        <input 
+          type="text" 
+          name="job" 
+          value={job} 
+          onChange={this.handleChange}/>
+      </form>
+	  )
+	}
 }
+
+export default Form;
