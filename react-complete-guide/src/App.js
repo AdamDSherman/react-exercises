e2f5283f0e4833ed7001050cf5de9155c15ef018
Adam <adam@Adams-MacBook-Air.local>
2020-01-03 11:20:07 +1000

    Tic tac toe tutorial

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const App = props => {
+
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 34 },
+      { name: 'Stephanie', age: 26  }
+    ],
+    otherState: 'some other value'
+  });
+
+  const [ otherState, setOtherState ] = useState('some other value again');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = (newName) => {
+    setPersonsState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 34 },
+        { name: 'Stephanie', age: 300  }
+      ]
+    });
+  };
+ 
   return (
     <div className="App">
       <h1>Hi i'm a react app</h1>
       <p>Other element</p>
+      <button onClick={() => switchNameHandler('this is a new name')}>Switch Name</button>
+      <Person 
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age} />
+      <Person
+       name={personsState.persons[1].name}
+       age={personsState.persons[1].age} 
+       click={switchNameHandler.bind(this, 'another new name')} >My hobbies: Racing</Person>
+      <Person
+       name={personsState.persons[2].name}
+       age={personsState.persons[2].age} />
     </div>
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi im a react app'));
   );
+
 }
 
 export default App;
